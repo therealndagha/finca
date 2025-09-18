592769028df11d0a97efe045ff7b137c5e3320d6
Ndagha Kang'oma <ndaghakangoma@gmail.com>
2025-09-18 08:18:32 +0200

    Add insurance, complaints, guarantor, writeoff, disbursement and more

--- server/src/index.js
+++ server/src/index.js
@@ -12,8 +12,13 @@ import loanApplicationRoutes from './routes/loanApplication-routes.js';
 import rolesRoutes from './routes/role-routes.js';
 import loanRoutes from './routes/loan-routes.js';
 import repaymentRouter from "./routes/repayment-routes.js";
-
-
+import collateralRoutes from './routes/collateralRoutes.js';
+import guarantorRoutes from './routes/guarantorRoutes.js';
+import loanDisbursementRoutes from './routes/loanDisbursementRoutes.js';
+import complaintsRoutes from './routes/complaintsRoutes.js';
+import paymentMethodRoutes from './routes/paymentMethodsRoutes.js';
+import loanWriteOffRoutes from './routes/loanWriteOffRoutes.js';
+import loanInsuranceRoutes from './routes/loanInsuranceRoutes.js';
 
 
 dotenv.config();
@@ -44,5 +49,12 @@ app.use('/api/loanapplications', loanApplicationRoutes);
 app.use('/api/role', rolesRoutes);
 app.use('/api/loans', loanRoutes);
 app.use("/api/repayments", repaymentRouter);
+app.use("/api/collateral", collateralRoutes);
+app.use('/api/guarantor', guarantorRoutes);
+app.use('/api/loandisbursements', loanDisbursementRoutes);
+app.use('/api/complaints', complaintsRoutes);
+app.use('/api/paymentmethods', paymentMethodRoutes);
+app.use('/api/loanwriteoff', loanWriteOffRoutes);
+app.use('/api/loaninsurance', loanInsuranceRoutes);
 
 app.listen(PORT, ()=>console.log(`server is listening on port: ${PORT}`));
